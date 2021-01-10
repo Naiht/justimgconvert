5f7493f3176096243d480adfb6be649bf87887c6
Cristhian Morales <cristhian.moralesf28@gmail.com>
2021-01-10 06:22:02 -0600

    actualizacion del ignore

---
+++ IMGConverter/IMGConverter.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace IMGConverter
+{
+    partial class IMGConverter
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(IMGConverter));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ListaCarga = new System.Windows.Forms.ListBox();
+            this.listaBox = new System.Windows.Forms.ComboBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnRemp = new System.Windows.Forms.Button();
+            this.btnInfo = new System.Windows.Forms.Button();
+            this.btnSelec = new System.Windows.Forms.Button();
+            this.btnConv = new System.Windows.Forms.Button();
+            this.lblimgc = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(68)))), ((int)(((byte)(173)))));
+            this.panel1.Controls.Add(this.pictureBox7);
+            this.panel1.Controls.Add(this.pictureBox6);
+            this.panel1.Controls.Add(this.pictureBox1);
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Name = "panel1";
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            // 
+            // pictureBox7
+            // 
+            resources.ApplyResources(this.pictureBox7, "pictureBox7");
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Click += new System.EventHandler(this.pictureBox7_Click);
+            // 
+            // pictureBox6
+            // 
+            resources.ApplyResources(this.pictureBox6, "pictureBox6");
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // ListaCarga
+            // 
+            this.ListaCarga.FormattingEnabled = true;
+            resources.ApplyResources(this.ListaCarga, "ListaCarga");
+            this.ListaCarga.Name = "ListaCarga";
+            // 
+            // listaBox
+            // 
+            resources.ApplyResources(this.listaBox, "listaBox");
+            this.listaBox.FormattingEnabled = true;
+            this.listaBox.Items.AddRange(new object[] {
+            resources.GetString("listaBox.Items"),
+            resources.GetString("listaBox.Items1"),
+            resources.GetString("listaBox.Items2"),
+            resources.GetString("listaBox.Items3"),
+            resources.GetString("listaBox.Items4"),
+            resources.GetString("listaBox.Items5"),
+            resources.GetString("listaBox.Items6"),
+            resources.GetString("listaBox.Items7")});
+            this.listaBox.Name = "listaBox";
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(133)))), ((int)(((byte)(48)))), ((int)(((byte)(170)))));
+            this.btnBuscar.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnBuscar, "btnBuscar");
+            this.btnBuscar.ForeColor = System.Drawing.Color.White;
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.UseVisualStyleBackColor = false;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnRemp
+            // 
+            this.btnRemp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(133)))), ((int)(((byte)(48)))), ((int)(((byte)(170)))));
+            this.btnRemp.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnRemp, "btnRemp");
+            this.btnRemp.ForeColor = System.Drawing.Color.White;
+            this.btnRemp.Name = "btnRemp";
+            this.btnRemp.UseVisualStyleBackColor = false;
+            // 
+            // btnInfo
+            // 
+            this.btnInfo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(133)))), ((int)(((byte)(48)))), ((int)(((byte)(170)))));
+            this.btnInfo.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnInfo, "btnInfo");
+            this.btnInfo.ForeColor = System.Drawing.Color.White;
+            this.btnInfo.Name = "btnInfo";
+            this.btnInfo.UseVisualStyleBackColor = false;
+            this.btnInfo.Click += new System.EventHandler(this.btnInfo_Click);
+            // 
+            // btnSelec
+            // 
+            this.btnSelec.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(133)))), ((int)(((byte)(48)))), ((int)(((byte)(170)))));
+            this.btnSelec.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnSelec, "btnSelec");
+            this.btnSelec.ForeColor = System.Drawing.Color.White;
+            this.btnSelec.Name = "btnSelec";
+            this.btnSelec.UseVisualStyleBackColor = false;
+            this.btnSelec.Click += new System.EventHandler(this.btnSelec_Click);
+            // 
+            // btnConv
+            // 
+            this.btnConv.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(133)))), ((int)(((byte)(48)))), ((int)(((byte)(170)))));
+            this.btnConv.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnConv, "btnConv");
+            this.btnConv.ForeColor = System.Drawing.Color.White;
+            this.btnConv.Name = "btnConv";
+            this.btnConv.UseVisualStyleBackColor = false;
+            this.btnConv.Click += new System.EventHandler(this.btnConv_Click);
+            // 
+            // lblimgc
+            // 
+            resources.ApplyResources(this.lblimgc, "lblimgc");
+            this.lblimgc.ForeColor = System.Drawing.Color.White;
+            this.lblimgc.Name = "lblimgc";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Name = "label2";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.pictureBox2, "pictureBox2");
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.pictureBox3, "pictureBox3");
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.pictureBox4, "pictureBox4");
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.pictureBox5, "pictureBox5");
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.TabStop = false;
+            // 
+            // IMGConverter
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(89)))), ((int)(((byte)(182)))));
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblimgc);
+            this.Controls.Add(this.btnConv);
+            this.Controls.Add(this.btnSelec);
+            this.Controls.Add(this.btnInfo);
+            this.Controls.Add(this.btnRemp);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.listaBox);
+            this.Controls.Add(this.ListaCarga);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "IMGConverter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListBox ListaCarga;
+        private System.Windows.Forms.ComboBox listaBox;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnRemp;
+        private System.Windows.Forms.Button btnInfo;
+        private System.Windows.Forms.Button btnSelec;
+        private System.Windows.Forms.Button btnConv;
+        private System.Windows.Forms.Label lblimgc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ IMGConverter/IMGConverter.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace IMGConverter
+{
+    public partial class IMGConverter : Form
+    {
+
+        List<string> list = new List<string>();
+
+        string res = "";
+
+        
+
+        public IMGConverter()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hwnd, int wmsg, int wparam, int lparam);
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listaBox.DropDownStyle = ComboBoxStyle.DropDownList;
+            btnRemp.Enabled = false;
+            btnConv.Enabled = false;
+            btnSelec.Enabled = false;
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            ListaCarga.Items.Clear();
+            list.Clear();
+            openFileDialog2.Multiselect = true;
+            openFileDialog2.Filter = "Jpg image(*.jpg)|*.jpg|" + "BmpPT image(*.bmp)|*.bmp|" + "Png image(*.png)|*.png|" +
+                "Gif image(*.gif)|*.gif|" + "Emf image(*.emf)|*.emf|" + "Exif image(*.exif)|*.exif|" + "Icon image(*.ico)|*.ico|" +
+                "Wmf image(*.wmf)|*.wmf|" + "Tiff image(*.tiff)|*.tiff|" + "All Files(*.*)|*.*";
+            if (openFileDialog2.ShowDialog() == DialogResult.OK)
+            {
+                string[] info = openFileDialog2.FileNames;
+                foreach (string s in info)
+                {
+                    ListaCarga.Items.Add(Path.GetFileName(s));
+                    list.Add(s);
+                }
+
+                btnRemp.Enabled = true;
+                btnSelec.Enabled = true;
+            }
+        }
+
+        private void btnInfo_Click(object sender, EventArgs e)
+        {
+            String info = "";
+            info = "IMG Converter v 1.0" + "\nDesarollado por Naiht 2019";
+            MessageBox.Show(info,"",MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+
+        private void btnSelec_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                res = folderBrowserDialog1.SelectedPath;
+            }
+            btnConv.Enabled = true;
+        }
+
+        private void btnConv_Click(object sender, EventArgs e)
+        {
+            String formato = "";
+            int f = 0;
+
+            if (listaBox.SelectedIndex >= 0)
+            {
+                formato = listaBox.Items[listaBox.SelectedIndex].ToString();
+
+                String ruta = "";
+
+                foreach (string s in list)
+                {
+                    Image img = Image.FromFile(@s);
+
+                    ruta = res + "\\" + Path.GetFileNameWithoutExtension(s) + "." + formato.ToLower();
+
+                    switch (formato)
+                    {
+                        case "Bmp":
+                            img.Save(@ruta, System.Drawing.Imaging.ImageFormat.Bmp);
+                            break;
+                        case "Jpg":
+                            img.Save(@ruta, System.Drawing.Imaging.ImageFormat.Jpeg);
+                            break;
+                        case "Gif":
+                            img.Save(@ruta, System.Drawing.Imaging.ImageFormat.Gif);
+                            break;
+                        case "Png":
+                            img.Save(@ruta, System.Drawing.Imaging.ImageFormat.Png);
+                            break;
+                        case "Emf":
+                            img.Save(@ruta, System.Drawing.Imaging.ImageFormat.Emf);
+                            break;
+                        case "Exif":
+                            img.Save(@ruta, System.Drawing.Imaging.ImageFormat.Exif);
+                            break;
+                        case "Ico":
+                            img.Save(@ruta, System.Drawing.Imaging.ImageFormat.Icon);
+                            break;
+                        case "Wmf":
+                            img.Save(@ruta, System.Drawing.Imaging.ImageFormat.Wmf);
+                            break;
+                        case "Tiff":
+                            img.Save(@ruta, System.Drawing.Imaging.ImageFormat.Tiff);
+                            break;
+                    }
+                    f++;
+                }
+
+                MessageBox.Show("Imagenes convertidas con exito.");
+            }
+            else {
+
+                MessageBox.Show("Seleccione el formato al que desea convertir las imagenes.");
+            }
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle,0x112,0xf012,0);
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+    }
+}
